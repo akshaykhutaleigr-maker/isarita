58b92df3f597ae464aa10691a88ca23b7b2c60cc
Akshay Shende <Akshayshendebca1@gmail.com>
2025-11-20 17:02:03 +0530

    added Css And bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Header from "./Components/header/Header"
 import Footer from "./Components/Footer/Footer"
 import Partners from "./Components/Partners/Partners";
 import Login from "./Components/Login/Login";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return(
     <>
